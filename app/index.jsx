7d800d1c6100336286056d9f1325a89e9df7b428
Shani Soni <134639145+Shanisoni@users.noreply.github.com>
2025-07-25 03:31:30 +0530

    Tailwind Configured

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,18 @@
 import { Text, View } from "react-native";
 
+
 export default function Index() {
   return (
     <View
+    className="flex-1 items-center justify-center bg-red-100"
       style={{
         flex: 1,
+
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text className="text-lg font-bold">Edit app/index.tsx to edit this screen
-
-        Shani Soni
+      <Text className="text-red-500">Shani Soni
       </Text>
     </View>
   );
