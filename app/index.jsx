61a4a0a85f2819d893b8e40f4b497dfd4a9e0502
Shani Soni <134639145+Shanisoni@users.noreply.github.com>
2025-07-25 12:18:27 +0530

    commited

--- app/index.jsx
+++ app/index.jsx
@@ -1,27 +1,72 @@
-import { Text, View , TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
-
-
+import {
+  Image,
+  ScrollView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import logo from "../assets/images/DineTimeLogo.png";
+const entryImg = require("../assets/images/Frame.png");
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// const logo = require("../assets/images/DineTimeLogo.png");
 export default function Index() {
   const router = useRouter();
-  return (
-    <View
-    className="flex-1 items-center justify-center bg-red-100"
-      style={{
-        flex: 1,
 
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-500">Shani Soni
-      <TouchableOpacity
-        className="bg-blue-500 p-4 rounded-lg"
-        onPress={() => router.push("/testing")}
-      >
-        <Text className="text-white">Go to Testing Page</Text>
-      </TouchableOpacity>
-      </Text>
-    </View>
+  const handleGuest = async () => {
+    await AsyncStorage.setItem("isGuest", "true");
+    router.push("/home");
+  };
+
+  return (
+    <SafeAreaView className={`bg-[#2b2b2b]`}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="m-2 flex justify-center items-center">
+          <Image source={logo} style={{ width: 300, height: 300 }} />
+          <View className="w-3/4">
+            <TouchableOpacity
+              onPress={() => router.push("/signup")}
+              className="p-2 my-2 bg-[#f49b33]  text-black rounded-lg "
+            >
+              <Text className="text-lg font-semibold text-center">Sign Up</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handleGuest}
+              className="p-2 my-2 bg-[#2b2b2b] border border-[#f49b33] rounded-lg max-w-fit "
+            >
+              <Text className="text-lg font-semibold text-[#f49b33] text-center">
+                Guest User
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text className="text-center text-base  font-semibold my-4 text-white">
+              <View className="border-b-2 border-[#f49b33] p-2 mb-1 w-24" /> or{" "}
+              <View className="border-b-2 border-[#f49b33] p-2 mb-1 w-24" />
+            </Text>
+
+            <TouchableOpacity
+              className="flex flex-row justify-center items-center"
+              onPress={() => router.push("/signin")}
+            >
+              <Text className="text-white font-semibold">Already a User? </Text>
+              <Text className="text-base font-semibold underline text-[#f49b33]">
+                Sign in
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View className="flex-1">
+          <Image
+            source={entryImg}
+            className="w-full h-full"
+            resizeMode="contain"
+          />
+        </View>
+        <StatusBar barStyle={"light-content"} backgroundColor={"#2b2b2b"} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
